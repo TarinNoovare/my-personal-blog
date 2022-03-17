54648f3625567d549db03076b73a3a33fc719b9a
Tarin Noovare <tarin.no@tnis.com>
2022-03-17 08:35:08 +0700

    Add tag-pills section for filtering contents

---
+++ src/components/TagsBar/TagPillsContainer.js
@@ -0,0 +1,38 @@
+import "./TagPillsContainer.css";
+
+function TagPillsContainer() {
+  const tagsPillList = [
+    "Travel",
+    "DataSci",
+    "Coding",
+    "Stats",
+    "Shitpost",
+    "ML",
+    "React",
+    "Life & Work",
+    "Books",
+    "P-value",
+    "Bruh",
+  ];
+  const tagsPillListComponent = [];
+
+  const onTagButtonClickHandler = (event) => {
+    console.log(event.target.value);
+  };
+
+  tagsPillList.forEach((tag) =>
+    tagsPillListComponent.push(
+      <button
+        className="tag-pill"
+        value={tag}
+        onClick={onTagButtonClickHandler}
+      >
+        {tag}
+      </button>
+    )
+  );
+
+  return <div className="tag-pills-container">{tagsPillListComponent}</div>;
+}
+
+export default TagPillsContainer;
