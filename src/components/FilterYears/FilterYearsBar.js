6645141840a899993be3757d857cf0ce34e2b069
Tarin Noovare <tarin.no@tnis.com>
2022-03-21 22:22:49 +0700

    Refactor Blog Cards Component and add datetime field into blog

---
+++ src/components/FilterYears/FilterYearsBar.js
@@ -0,0 +1,15 @@
+import "./FilterYearsBar.css"
+
+function FilterYearsBar() {
+  return (
+    <div>
+      <select>
+        <option value="tesla">Tesla</option>
+        <option value="volvo">Volvo</option>
+        <option value="mercedes">Mercedes</option>
+      </select>
+    </div>
+  );
+}
+
+export default FilterYearsBar;
--- src/components/MainBlogCards/BlogsContainer.js
+++ src/components/MainBlogCards/BlogsContainer.js
@@ -1,55 +1,28 @@
 import "./BlogsContainer.css";
 
-import BlackCatImage from "../../resources/cat.jpeg";
-import MySelfImage from "../../resources/myself.jpeg";
-import FitnessImage from "../../resources/fitness_first.jpeg";
-import OmuRiceImage from "../../resources/omu.jpeg";
+function BlogsContainer(props) {
+  const blogsItemDict = props.blogsItemDict;
 
-function BlogsContainer() {
-  return (
-    <section className="blog-container">
+  const blogsComponents = [];
+  blogsItemDict.forEach((blog) =>
+    blogsComponents.push(
       <article className="blog-card">
         <div className="blog-card__picture">
-          <img src={MySelfImage} alt="Myself" />
+          <img src={blog.pictureSourcePath} alt={blog.blogHeader} />
         </div>
         <div className="blog-card__desc">
-          <div className="blog-card__desc-header">Myself</div>
-          <div className="blog-card__desc-main">Picture of Myself, selfied by using my New iPad Mini!</div>
-        </div>
-      </article>
-      <article className="blog-card">
-        <div className="blog-card__picture">
-          <img src={BlackCatImage} alt="A Black Cat" />
-        </div>
-        <div className="blog-card__desc">
-          <div className="blog-card__desc-header">Black Cat</div>
-          <div className="blog-card__desc-main">Picture of a black cat wearing a motorcycle taxi trying to earn for his meal.</div>
-        </div>
-      </article>
-
-      <article className="blog-card">
-        <div className="blog-card__picture">
-          <img src={FitnessImage} alt="Fitness View" />
-        </div>
-        <div className="blog-card__desc">
-          <div className="blog-card__desc-header">Fitness First @SiamParagon</div>
-          <div className="blog-card__desc-main">
-              Fitness First View at Siam Paragon. So much spacious here!
+          <div className="blog-card__desc-header">
+            
+            <div className="blog-card__desc-datetime">{blog.blogDatetime}</div>
+            <div>{blog.blogHeader}</div>
           </div>
+          <div className="blog-card__desc-main">{blog.blogDescription}</div>
         </div>
       </article>
-
-      <article className="blog-card">
-        <div className="blog-card__picture">
-          <img src={OmuRiceImage} alt="Omu Rice" />
-        </div>
-        <div className="blog-card__desc">
-          <div className="blog-card__desc-header">My First Omu Rice</div>
-          <div className="blog-card__desc-main">My First Japanese-like Omu Rice at Central World</div>
-        </div>
-      </article>
-    </section>
+    )
   );
+
+  return <section className="blog-container">{blogsComponents}</section>;
 }
 
 export default BlogsContainer;
