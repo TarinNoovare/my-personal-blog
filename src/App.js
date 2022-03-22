5624b603a2d104c09d3a1ac541245f36040a6252
Tarin Noovare <tarin.no@tnis.com>
2022-03-22 23:32:00 +0700

    Edit data for testing to be a stand-alone js file

--- src/App.js
+++ src/App.js
@@ -6,56 +6,10 @@ import MainSectionContainer from "./components/MainBlogCards/MainSectionContaine
 
 import { useState } from "react";
 
+import { DUMMY_BLOG_ITEMS } from "./resources/ItemsForTesting";
+
 function App() {
-  const blogsItemDict = [
-    {
-      id: 0,
-      blogHeader: "Black Cat",
-      pictureSourcePath: require("./resources/cat.jpeg"),
-      blogDescription:
-        "Picture of a black cat wearing a motorcycle taxi trying to earn for his meal.",
-      blogDatetime: "2018-01-01",
-    },
-    {
-      id: 1,
-      blogHeader: "Black Cat",
-      pictureSourcePath: require("./resources/cat.jpeg"),
-      blogDescription:
-        "Picture of a black cat wearing a motorcycle taxi trying to earn for his meal.",
-      blogDatetime: "2021-01-01",
-    },
-    {
-      id: 2,
-      blogHeader: "Myself",
-      pictureSourcePath: require("./resources/myself.jpeg"),
-      blogDescription: "Picture of Myself, selfied by using my New iPad Mini!",
-      blogDatetime: "2022-01-01",
-    },
-    {
-      id: 3,
-      blogHeader: "Fitness First @SiamParagon",
-      pictureSourcePath: require("./resources/fitness_first.jpeg"),
-      blogDescription:
-        "Fitness First View at Siam Paragon. So much spacious here!",
-      blogDatetime: "2023-06-01",
-    },
-    {
-      id: 4,
-      blogHeader: "Omu Rice",
-      pictureSourcePath: require("./resources/omu.jpeg"),
-      blogDescription:
-        "My First Japanese-like Omu Rice at Central World. Yum!!",
-      blogDatetime: "2023-02-01",
-    },
-    {
-      id: 5,
-      blogHeader: "Omu Rice",
-      pictureSourcePath: require("./resources/omu.jpeg"),
-      blogDescription:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      blogDatetime: "2021-12-15",
-    },
-  ];
+  const [blogsItemDict, setBlogsItemDict] = useState(DUMMY_BLOG_ITEMS);
 
   const filterBlogYear = (year) => {
     setYearBlogs(year);
---
+++ src/resources/ItemsForTesting.js
@@ -0,0 +1,50 @@
+const DUMMY_BLOG_ITEMS = [
+  {
+    id: 0,
+    blogHeader: "Black Cat",
+    pictureSourcePath: require("./cat.jpeg"),
+    blogDescription:
+      "Picture of a black cat wearing a motorcycle taxi trying to earn for his meal.",
+    blogDatetime: "2018-01-01",
+  },
+  {
+    id: 1,
+    blogHeader: "Black Cat",
+    pictureSourcePath: require("./cat.jpeg"),
+    blogDescription:
+      "Picture of a black cat wearing a motorcycle taxi trying to earn for his meal.",
+    blogDatetime: "2021-01-01",
+  },
+  {
+    id: 2,
+    blogHeader: "Myself",
+    pictureSourcePath: require("./myself.jpeg"),
+    blogDescription: "Picture of Myself, selfied by using my New iPad Mini!",
+    blogDatetime: "2022-01-01",
+  },
+  {
+    id: 3,
+    blogHeader: "Fitness First @SiamParagon",
+    pictureSourcePath: require("./fitness_first.jpeg"),
+    blogDescription:
+      "Fitness First View at Siam Paragon. So much spacious here!",
+    blogDatetime: "2023-06-01",
+  },
+  {
+    id: 4,
+    blogHeader: "Omu Rice",
+    pictureSourcePath: require("./omu.jpeg"),
+    blogDescription: "My First Japanese-like Omu Rice at Central World. Yum!!",
+    blogDatetime: "2023-02-01",
+  },
+  {
+    id: 5,
+    blogHeader: "Omu Rice",
+    pictureSourcePath: require("./omu.jpeg"),
+    blogDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    blogDatetime: "2021-12-15",
+  },
+];
+
+export { DUMMY_BLOG_ITEMS };
