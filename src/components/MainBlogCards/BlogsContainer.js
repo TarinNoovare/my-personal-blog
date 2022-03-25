0312ff51f93ee91f098ef02cfa958d7dfe301899
Tarin Noovare <tarin.no@tnis.com>
2022-03-25 13:05:21 +0700

    Fix filter year module to be more functioning way

--- src/components/MainBlogCards/BlogsContainer.js
+++ src/components/MainBlogCards/BlogsContainer.js
@@ -4,19 +4,16 @@ import { useState } from "react";
 function BlogsContainer(props) {
   const blogsItemDict = props.blogsItemDict;
 
-  const blogsComponents = [];
-  blogsItemDict.forEach((blog) => {
-    const isCardShow =
+  const filteredBlogsItemDict = blogsItemDict.filter((blog) => {
+    return (
       props.filterYear === "All" ||
       (props.filterYear !== "All" &&
-        blog.blogDatetime.slice(0, 4) === props.filterYear);
-
-    blogsComponents.push(
-      <article
-        className="blog-card"
-        style={{ display: isCardShow ? "flex" : "none" }}
-        key={blog.id}
-      >
+        blog.blogDatetime.slice(0, 4) === props.filterYear)
+    );
+  });
+  const blogsComponents = filteredBlogsItemDict.map((blog) => {
+    return (
+      <article className="blog-card" key={blog.id}>
         <div className="blog-card__picture">
           <img src={blog.pictureSourcePath} alt={blog.blogHeader} />
         </div>
